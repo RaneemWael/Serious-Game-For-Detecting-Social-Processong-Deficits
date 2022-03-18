e5911a3976dad245ab598071697a3e8618309fba
Raneem Wael <raneemwael99@gmail.com>
2022-03-18 20:57:36 +0200

    Add files via upload

---
+++ Assets/Prefabs/MiniGames/Pipe/GameInspector.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInspector : MonoBehaviour
+{
+    //array where all the pipe objects in the scene are stored
+    private PipeScript[] pipeObj;
+    public bool levelComplete = false;
+
+    public GameObject game;
+    public GameObject finished;
+
+    void Start()
+    {
+        //get all the pipeObj and are stored in array
+        pipeObj = FindObjectsOfType<PipeScript>();
+    }
+
+    void Update()
+    {
+        CheckForLevelComplete();
+        if (levelComplete)
+        {
+            game.SetActive(false);
+            finished.SetActive(true);
+        }
+    }
+
+    void CheckForLevelComplete()
+    {
+        for (int i = 0; i < pipeObj.Length; i++)
+        {
+            if (pipeObj[i].completed == false)
+            {
+                levelComplete = false;
+                return;
+            }
+        }
+        levelComplete = true;
+    }
+}
---
+++ Assets/Prefabs/MiniGames/Pipe/PipeScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class PipeScript : MonoBehaviour
+{
+    [Header("Used to check the piece rotation")]
+    [SerializeField]
+    private Vector3 currentAngle;    //this the angle the piece is currently in the scene
+    [SerializeField]
+    private Vector3 originalAngle;    //this the angle the piece need to complete level
+    private Vector3 targetAngle;
+
+    public int rotationDirection = -1; // -1 for clockwise & 1 for anti-clockwise
+    public int rotationStep = 10; // should be less than 90
+    private bool rotating = false;
+    
+    public bool completed = false;  
+
+    private Vector3 currentRotation;
+    private Vector3 targetRotation;
+    
+    void Awake()
+    {
+        completed = false;
+    }
+
+
+    void Start()
+    {
+        //currentAngle = gameObject.transform.eulerAngles;
+    }
+
+    void Update()
+    {
+        currentAngle = gameObject.transform.eulerAngles;
+
+        if (gameObject.transform.rotation.z == 0.0f)
+        {
+            completed = true;
+        }
+        else
+        {
+            completed = false;
+        }
+    }
+
+    public void RotateObj()
+    {
+        currentAngle = gameObject.transform.eulerAngles;
+        targetAngle.z = currentAngle.z + (90 * rotationDirection);
+        transform.DOLocalRotate(targetAngle, 0.2f);
+    }
+    
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0) && !rotating)
+        {
+            rotating = true;
+            gameObject.GetComponent<AudioSource>().Play();
+            RotateObj();
+            StartCoroutine(RotatingTrue());
+        }
+    }
+
+    //for limiting the number of taps per second
+    IEnumerator RotatingTrue()
+    {
+        yield return new WaitForSeconds(0.25f);
+        rotating = false;
+    }
+}
